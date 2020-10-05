0c8d0142b717924b63f4d19d8c3cd7120df8a259
Thomas Herzog <thomas@TomsLaptop.WORKGROUP>
2020-10-05 14:27:10 -0700

    Add .gitconfig

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ BASEDIR=$(dirname "$0")
 # *Use -sf if you would like to overwrite your current config files.
 ln -s $BASEDIR/dotfile/.bash_aliases ~/.bash_aliases
 ln -s $BASEDIR/dotfile/.bashrc ~/.bashrc
+ln -s $BASEDIR/dotfile/.gitconfig ~/.gitconfig
