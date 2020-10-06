967dfb38467c3e9e42cec3a7cd9125d6fc57ac7f
Thomas Herzog <Tomrule007@gmail.com>
2020-10-06 15:17:02 -0700

    Link VS Code config files / add to install script

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ SCRIPT_PATH=$(dirname $(readlink -f "$0"))
 ln -s $SCRIPT_PATH/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s $SCRIPT_PATH/dotfiles/.bashrc ~/.bashrc
 ln -s $SCRIPT_PATH/dotfiles/.gitconfig ~/.gitconfig
+
+# VSCODE Files
+ln -s $SCRIPT_PATH/dotfiles/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s $SCRIPT_PATH/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
+ln -s -t ~/.config/Code/User/ $SCRIPT_PATH/dotfiles/.config/Code/User/snippets/ 
+
